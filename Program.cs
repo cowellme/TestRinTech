21883a5e1d1a79990b365b0888fe839b6b3c716c
Poma <Poma@HOME-PC>
2025-09-29 18:25:19 +0300

    Completed 2 Task

--- Program.cs
+++ Program.cs
@@ -1,13 +1,125 @@
-﻿public class Program
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using TestRinTech;
+
+public class Program
 {
     public static void Main()
     {
         // В задании осознано не используется асинхронное прораммирование, поскольку я посчитал,
         // что это излишество для консоли
+        
+        
+        Console.WriteLine("1 - Лифт\n" +
+                          "2 - Математика\n" +
+                          "3 - Возрасты");
 
+        if (int.TryParse(Console.ReadLine(), out var cmd))
+        {
+            switch (cmd)
+            {
+                case 1: Elevator(); break;
+                case 2: RinMaths(); break;
+                case 3: Years(); break;
+                default: Elevator(); break;
+            }
+        }
+    }
+
+    private static void Elevator()
+    {
         var elevator = new Elevator(-3, 26);
         var isWork = true;
-        while (isWork) 
+        while (isWork)
+        {
+            var command = Console.ReadLine();
+            if (int.TryParse(command, out var floor))
+            {
+                var result = elevator.move(floor);
+                Console.WriteLine(result ? "Успешно\n" : "Этаж не доступен\n");
+            }
+            else if (command == "e")
+            {
+                isWork = false;
+            }
+        }
+    }
+
+    private static void RinMaths()
+    {
+        var isWork = true;
+        while (isWork)
+        {
+            Console.WriteLine("1. Площадь кругат\n" +
+                              "2. Площадь треугольника\n" +
+                              "3. Объём шара\n" +
+                              "4. Проверка треугольника\n\n" +
+                              "Введите что надо расчитать: ");
+            var command = Console.ReadLine();
+            double result = .0;
+            switch (command)
+            {
+                case "1":
+                    var data1 = parseOneNumber();
+                    result = RinMath.circleArea(data1);
+                    Console.WriteLine($"Площадь круга: {result}\n");
+                    break;
+                case "2":
+                    var data2 = parseThreeNumber();
+                    result = RinMath.triangleArea(data2[0], data2[1], data2[2]);
+                    Console.WriteLine($"Площадь три-ка: {result}\n");
+                    break;
+                    
+                case "3": 
+                    var data3 = parseOneNumber();
+                    result = RinMath.sphereVolume(data3);
+                    Console.WriteLine($"Объем сферы: {result}\n");
+                    break;
+                case "4":
+                    var data4 = parseThreeNumber();
+                    Console.WriteLine($"{RinMath.validTriangle(data4[0], data4[1], data4[2])}\n");
+                    break;
+
+            }
+        }
+    }
+
+    private static double parseOneNumber()
+    {
+        if (double.TryParse(Console.ReadLine(), out var result))
+        {
+            return result;
+        }
+        throw new Exception("Ошибка ввода");
+
+    }
+    private static List<double> parseThreeNumber()
+    {
+        var listResult = new List<double>();
+        var strs = Console.ReadLine()?.Split(",").ToList();
+        if (strs != null)
+        {
+            foreach (var str in strs)
+            {
+                if (double.TryParse(str, out var result))
+                {
+                    listResult.Add(result);
+                }
+                
+            }
+
+            if (listResult.Count == 3) return listResult;
+        }
+        throw new Exception("Ошибка ввода");
+    }
+
+
+
+    private static void Years()
+    {
+        var elevator = new Elevator(-3, 26);
+        var isWork = true;
+        while (isWork)
         {
             var command = Console.ReadLine();
             if (int.TryParse(command, out var floor))
---
+++ RinMath.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TestRinTech
+{
+   
+    public static class RinMath
+    {
+       
+        public static double circleArea(double radius)
+        {
+            var result = -1.0;
+
+            if (radius <= 0)
+                Console.WriteLine("Радиус должен быть положительный\n");
+            else
+                result = Math.PI * radius * radius;
+
+            return result;
+        }
+
+        public static double triangleArea(double a, double b, double c)
+        {
+            var result = -1.0;
+            if (!validTriangle(a, b, c))
+                Console.WriteLine("Невозможно построить треугольник\n");
+            else
+            {
+                double s = (a + b + c) / 2.0;
+                result = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            }
+
+            return result;
+        }
+        
+        public static double sphereVolume(double radius)
+        {
+            var result = -1.0;
+            if (radius <= 0)
+                Console.WriteLine("Радиус должен быть положительный\n");
+            else
+                result = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+            return result;
+        }
+        
+        public static bool validTriangle(double a, double b, double c)
+        {
+            var result = true;
+            if (a <= 0 || b <= 0 || c <= 0)
+                result = false;
+            else
+                result = (a + b > c) && (a + c > b) && (b + c > a);
+            return result;
+        }
+    }
+}
